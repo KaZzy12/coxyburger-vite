c4185d0096daa85febd21413f36ce85c78b15c59
KaZzy12 <xthel0@gmail.com>
2024-01-15 08:31:04 +0100

    contact page styling

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,29 +1,26 @@
 <template>
-    <div class="section" id="contact">
-        <h2>Contact</h2>
+    <div class="h-screen w-screen flex flex-col items-center justify-center" id="contact">
         <div class="adress-container">
-            <h3>Coxyburger</h3>
+            <h3 class="text-xl text-center font-bold">Coxyburger</h3>
             <p>Rue du Baron d'Obin 132</p>
             <p>4219 Wasseiges</p>
             <p>Ouvert 7/7 de 17h30 à 21h</p>
             <p>et le mercredi de 11h30 à 13h30</p>
             <p>0489/18.45.70</p>
         </div>
-        <div>
-            <a href="https://maps.app.goo.gl/aLX5KuAphy4vmi5M8">
-              <img class="social" src="../assets/maps.png">
+        <div class="flex flex-row">
+            <a href="https://maps.app.goo.gl/aLX5KuAphy4vmi5M8" class="hover:brightness-75">
+              <img class="w-12" src="../assets/maps.png">
             </a>
-            <a href="https://www.facebook.com/coxyburger">
-              <img class="social" src="../assets/facebook.png">
+            <a href="https://www.facebook.com/coxyburger" class="hover:brightness-75">
+              <img class="w-12" src="../assets/facebook.png">
             </a>
-            <a href="https://www.m.me/100013961950313">
-              <img class="social" src="../assets/messenger.png">
+            <a href="https://www.m.me/100013961950313" class="hover:brightness-75">
+              <img class="w-12" src="../assets/messenger.png">
             </a>
-            <a href="https://api.whatsapp.com/send?phone=32489184570">
-              <img class="social" src="../assets/whatsapp.png">
+            <a href="https://api.whatsapp.com/send?phone=32489184570" class="hover:brightness-75">
+              <img class="w-12" src="../assets/whatsapp.png">
             </a>
-        </div>
-        <div class="socials-container">
         </div>
     </div>
 </template>
@@ -37,9 +34,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.social {
-  width: 50px;
-}
-</style>
