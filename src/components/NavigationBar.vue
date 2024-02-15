2ee15a5d48ea6931a777a12f87140fa671f6249b
KaZzy12 <xthel0@gmail.com>
2024-02-15 13:50:11 +0100

    hamburger-menu setup

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar flex w-auto p-3 items-center justify-between border-x-2 rounded-xl text-white text-xl sticky">
+    <nav class="navbar flex p-3 items-center justify-between md:border-x-2 md:rounded-xl text-white text-xl sticky">
       <div class="flex flex-wrap">
         <router-link
          :to="{name: 'Home'}"
@@ -8,33 +8,74 @@
          <span class="!text-white">Coxyburger</span>
         </router-link>
       </div>
-      <button type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-      </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row rtl:space-x-reverse md:mt-0 md:border-0">
-          <li>
-            <router-link class="inline-block md:mr-4 hover:text-red-500" :to="{name: 'Burgers'}" exact>
-              Burgers
-            </router-link>
-          </li>
-          <li>
-            <router-link class="inline-block md:mr-4 hover:text-red-500" :to="{name: 'Carte'}" exact>
-              Carte
-            </router-link>
-          </li>
-          <li>
-            <router-link class="inline-block hover:text-red-500" :to="{name: 'Contact'}" exact>
-              Contact
-            </router-link>
-          </li>
-        </ul>
+      <div>
+        <button id="hamburger-button" class="text-3xl md:hidden cursor-pointer">
+          &#9776
+        </button>
+        <nav class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row rtl:space-x-reverse md:mt-0 md:border-0">
+            <li>
+              <router-link class="inline-block md:mr-4 hover:text-red-500" :to="{name: 'Burgers'}" exact>
+                Burgers
+              </router-link>
+            </li>
+            <li>
+              <router-link class="inline-block md:mr-4 hover:text-red-500" :to="{name: 'Carte'}" exact>
+                Carte
+              </router-link>
+            </li>
+            <li>
+              <router-link class="inline-block hover:text-red-500" :to="{name: 'Contact'}" exact>
+                Contact
+              </router-link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </nav>
+    <section id="mobile-menu" 
+     class="absolute z-10 top-0 bg-[#234b90] sticky w-full h-full text-5xl flex-col justify-content-center origin-top animate-open-menu hidden">
+      <button class="text-white text-7xl self-end px-6">
+        &times;
+      </button>
+      <nav class="flex flex-col h-full items-center py-8" aria-label="mobile">
+        <router-link
+         :to="{name: 'Home'}"
+         class="text-white w-full text-center py-6 hover:opacity-90 hover:text-red-500">
+          Accueil
+        </router-link>
+        <router-link 
+         :to="{name: 'Burgers'}" exact
+         class="text-white w-full text-center py-6 hover:opacity-90 hover:text-red-500">
+          Burgers
+        </router-link>
+        <router-link 
+         :to="{name: 'Carte'}" exact
+         class="text-white w-full text-center py-6 hover:opacity-90 hover:text-red-500">
+          Carte
+        </router-link>
+        <router-link 
+         :to="{name: 'Contact'}" exact
+         class="text-white w-full text-center py-6 hover:opacity-90 hover:text-red-500">
+          Contact
+        </router-link>
+      </nav>
+    </section>
 </template>
+<script defer>
+  const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+    const toggleMenu = () => {
+      mobileMenu.classList.toggle('hidden')
+      mobileMenu.classList.toggle('flex')
+    }
+    hamburgerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+  }
+
+  document.addEventListener('DOMContentLoaded', initApp)
+</script>
 
 <style scoped>
 .navbar {
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ export default {
       colors: {
         burgundy: '#922028',
       },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)'},
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)'},
+          '80%': { transform: 'scaleY(1.2)'},
+          '100%': { transform: 'scaleY(1)'},
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
     },
   },
   plugins: [],
