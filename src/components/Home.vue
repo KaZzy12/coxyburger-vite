93dcec20833344d94ff995b1c0bd82e5013e1746
KaZzy12 <xthel0@gmail.com>
2024-06-04 07:44:02 +0200

    npm update + fix bg center + change picture quality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="contact" class="-z-50 -mt-[104px] lg:-mt-[136px] bg-cover h-screen w-screen" style=" background-image: url(images/homeimage.jpg);">
+  <section id="contact" class="-z-50 -mt-[104px] lg:-mt-[136px] bg-cover bg-center lg:bg-left-top h-screen w-screen" style=" background-image: url(images/homeimage.jpg);">
     <div class="h-full w-full flex flex-col items-center justify-center">
       <div class="absolute text-5xl text-white drop-shadow-2xl">  
         Bienvenue chez Coxyburger, là où la gourmandise prend tout son sens.
